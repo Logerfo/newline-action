20d5760d03b7f8d85381dda05025d92181184a09
Bruno Logerfo <bclogerfo@hotmail.com>
2019-10-24 10:05:14 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,70 @@
+[![Dependencies Status](https://david-dm.org/logerfo/newline-action/dev-status.svg)](https://david-dm.org/logerfo/newline-action?type=dev)
+
+# Newline Action
+This action will automatically fix files without a new line at the end in pull requests.  
+Only works for UTF-8 files.
+
+## Setting up
+Create a file named `.github/workflows/newline.yml`.
+
+### Minimal configuration
+```yml
+name: New line at the end of files
+on:
+  pull_request:
+    types: [synchronize, opened]
+jobs:
+  build:
+    name: New line
+    runs-on: ubuntu-16.04
+    steps:
+      - uses: actions/checkout@master
+      - uses: Logerfo/newline-action@0.0.1
+        env:
+          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }} # The `GITHUB_TOKEN` secret.
+```
+
+### Complete configuration
+All values are default.
+```yml
+name: New line at the end of files
+on:
+  pull_request:
+    types: [synchronize, opened]
+jobs:
+  build:
+    name: New line
+    runs-on: ubuntu-16.04
+    steps:
+      - uses: actions/checkout@master
+      - uses: Logerfo/newline-action@0.0.1
+        env:
+          GITHUB_TOKEN: ${{ secrets.GITHUB_TOKEN }} # The `GITHUB_TOKEN` secret.
+          config-path: .github/newline.yml
+```
+
+### Additional configurations file
+You can optionally create a additional configuration file. The default path is `.github/newline.yml`, but you can change it in the action configuration file, as shown above.  
+All values are default.
+```yml
+autoCommit: true # If false, the bot will still comment in the pull request, so you can known which files are wrong and fix them yourself.
+ignorePaths: ["bin/**", "node_modules/**", "out/**"] # Globs to be ignored
+```
+
+### Auto update
+You can use (at your own risk) the `release` branch instead of the specific version tag.  
+Never user `master`, since the distribution file does not exist in this branch and the action will always fail.
+
+## Changelog
+Click [here](CHANGELOG.md).
+
+## Contributing
+If you have suggestions for how close-label could be improved, or want to report a bug, open an issue! We'd love all and any contributions.
+
+For more, check out the [Contributing Guide](CONTRIBUTING.md).
+
+## Donate
+
+<img src="https://i.imgur.com/ndlBtuX.png" width="200">
+
+BTC: 1LoGErFoNzE1gCA5fzk6A82nV6iJdKssSZ
---
+++ src/main.js
@@ -0,0 +1,233 @@
+#!/usr/bin/env node
+
+const path = require("path");
+const fs = require("fs").promises;
+const yaml = require("js-yaml");
+const util = require("util");
+const glob = util.promisify(require("glob"));
+const { isText } = require('istextorbinary')
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+const REPO_DIRECTORY = process.env["GITHUB_WORKSPACE"],
+    CONFIG_PATH = path.join(REPO_DIRECTORY, getInput("config-path", ".github/newline.yml")),
+    token = core.getInput("github-token", { required: true }),
+    context = github.context,
+    owner = context.repo.owner,
+    repo = context.repo.repo,
+    client = new github.GitHub(token),
+    committer = {
+        name: 'Bruno Logerfo',
+        email: 'bruno@logerfo.tk',
+    };
+
+const getEvent = async () => JSON.parse(await fs.readFile(process.env["GITHUB_EVENT_PATH"]));
+
+async function getYamlConfig() {
+    try {
+        const text = await fs.readFile(CONFIG_PATH);
+        return yaml.safeLoad(text);
+    }
+    catch (err) {
+        core.debug(err);
+        return undefined;
+    }
+}
+
+async function getConfig() {
+    const defaultConfig = {
+        autoCommit: true,
+        ignorePaths: [
+            "bin/**",
+            "node_modules/**",
+            "out/**",
+        ],
+    },
+        ymlConfig = await getYamlConfig(),
+        config = ymlConfig ? Object.assign(defaultConfig, ymlConfig) : defaultConfig;
+
+    core.info(ymlConfig ? "Config file loaded." : "Config file not found. Using default...");
+    core.debug(config);
+    return config;
+}
+
+function getInput(name, fallback) {
+    const input = core.getInput(name);
+    return input || fallback;
+}
+
+async function run() {
+    try {
+        core.debug(JSON.stringify(context.payload));
+        if (github.context.eventName != "pull_request") {
+            core.info("This action is supposed to run for pushes to pull requests only. Skipping...");
+            return;
+        }
+        const event = await getEvent();
+        if (!["synchronize", "opened"].includes(event.action)) {
+            core.info("This action is supposed to run for pushes to pull requests only. Skipping...");
+            return;
+        }
+        await push();
+    }
+    catch (err) {
+        //Even if it's a valid situation, we want to fail the action in order to be able to find the issue and fix it.
+        core.setFailed(err.message);
+    }
+}
+
+function getLineBreakChar(string) {
+    const indexOfLF = string.indexOf('\n', 1);  // No need to check first-character
+
+    if (indexOfLF === -1) {
+        if (string.indexOf('\r') !== -1) {
+            return '\r';
+        }
+        return '\n';
+    }
+
+    if (string[indexOfLF - 1] === '\r') {
+        return '\r\n';
+    }
+
+    return '\n';
+}
+
+async function processFiles(config) {
+    const paths = await glob(`${REPO_DIRECTORY}/**`, {
+        ignore: config.ignorePaths.map(p => path.resolve(REPO_DIRECTORY, p)),
+        nodir: true
+    }),
+        files = [];
+    let page = 0,
+        changedFiles;
+    core.info("Looking for changed files...");
+    do {
+        core.info(`Page ${++page}:`);
+        changedFiles = await client.pulls.listFiles({
+            owner,
+            page,
+            pull_number: context.payload.pull_request.number,
+            repo,
+        });
+        core.debug(changedFiles.data);
+        changedFiles.data.forEach(async function (element) {
+            if (!paths.includes(element.filename)) {
+                core.info(`${element.filename} is ignored. Skipping...`);
+                return;
+            }
+            if (!isText(element.filename)) {
+                core.info(`${element.filename} is not a text file. Skipping...`);
+                return;
+            }
+            const file = await fs.readFile(element.filename, { encoding: "utf8" });
+            if (file.endsWith("\n") || file.endsWith("\r")) {
+                core.info(`${element.filename} is not compromised. Skipping...`);
+                return;
+            }
+            core.info(`${element.filename} is compromised. Fixing...`);
+            const newFile = file.concat(getLineBreakChar(file));
+            await fs.writeFile(element.filename, newFile);
+            files.push(element.filename);
+        });
+    } while (changedFiles.data.length == 100);
+    return files;
+}
+
+async function generateMarkdownReport(results, autoCommit) {
+    return `
+${results.length} file(s) ${autoCommit ? "had their final line ending fixed" : "are missing a line break at their end"}:
+${results.foreach(function (element) {
+        return `- \`${element}\`\n`;
+    })}`;
+}
+
+async function convertToTreeBlobs(results) {
+    const blobs = [];
+    for (const path of results) {
+        const content = await fs.readFile(path, { encoding: "utf8" });
+        const blob = await client.git.createBlob({
+            content,
+            encoding: "utf8",
+            owner,
+            repo,
+        });
+        core.debug(blob.data);
+        blobs.push({
+            path: path.replace(REPO_DIRECTORY, "").replace(/\//, ""),
+            mode: "100644",
+            sha: blob.data.sha,
+            type: "blob",
+        });
+    }
+    return blobs;
+}
+
+async function createCommit(results) {
+    const head = context.ref.substr(11);
+    const latestCommit = await client.git.getCommit({
+        commit_sha: context.sha,
+        owner,
+        repo,
+    });
+    core.debug(latestCommit.data);
+    const tree = await client.git.createTree({
+        base_tree: latestCommit.data.tree.sha,
+        owner,
+        repo,
+        tree: await convertToTreeBlobs(results),
+    });
+    core.debug(tree.data);
+    const commit = await client.git.createCommit({
+        message: "Fixed final line endings with Logerfo/newline-action.",
+        owner,
+        repo,
+        tree: tree.data.sha,
+        parents: [context.sha]
+    });
+    core.debug(commit.data);
+    const update = await client.git.updateRef({
+        owner,
+        repo,
+        ref: `heads/${head}`,
+        sha: commit.data.sha,
+    });
+    core.debug(update.data);
+}
+
+async function createComment(body) {
+    const comment = await client.issues.createComment({
+        body,
+        issue_number: context.payload.pull_request.number,
+        owner,
+        repo,
+    });
+    core.debug(comment.data);
+}
+
+async function push() {
+    const config = await getConfig()
+
+    core.info("Locating files...");
+    const results = await processFiles(config);
+    if (!results.length) {
+        core.info("No compromised files found. Skipping...");
+        return;
+    }
+
+    core.info("Generating markdown report...");
+    const markdown = await generateMarkdownReport(results, config.autoCommit);
+
+    if (config.autoCommit) {
+        core.info("Committing files...");
+        await createCommit(results);
+    }
+    else {
+        core.info("Auto commit is disabled. Skipping...");
+    }
+
+    core.info("Leaving comment on PR...");
+    await createComment(markdown);
+}
+
+run();
